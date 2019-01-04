cd4d2194bebc429ef74749f96fde4bda93cbd8f6
alanlenni <alan.lenni@gmail.com>
2019-01-04 08:40:20 +0100

    1.5 with link error fix (libvideo)

--- WavToMP3/MainForm.cs
+++ WavToMP3/MainForm.cs
@@ -15,7 +15,7 @@ namespace WavToMP3
         public MainForm()
         {
             InitializeComponent();
-            Text = "Wav2Mp3 v1.4";
+            Text = "Wav2Mp3 v1.5";
             textBox2.Text = "https://www.youtube.com/watch?v=t-hTrgAJXTs";
         }
 
